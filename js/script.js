05dfab419bc5eed6ad089201f3e0443ae3159b0c
uconnecting <aislinn.walters@gmail.com>
2014-09-15 16:45:57 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,40 @@
 // create the module and name it scotchApp
-var psrApp = angular.module('psrApp', []);
+// also include ngRoute for all our routing needs
+var psrApp = angular.module('psrApp', ['ngRoute']);
 
-// create the controller and inject Angular's $scope
-psrApp.controller('mainController', function($scope) {
+	// configure our routes
+	psrApp.config(function($routeProvider) {
+		$routeProvider
 
-	// create a message to display in our view
-  $scope.message = 'Yo dudez!';
-});
+			// route for the home page
+			.when('/', {
+				templateUrl : 'pages/home.html',
+				controller  : 'mainController'
+			})
 
+			// route for the about page
+			.when('/about', {
+				templateUrl : 'pages/about.html',
+				controller  : 'aboutController'
+			})
+
+			// route for the contact page
+			.when('/contact', {
+				templateUrl : 'pages/contact.html',
+				controller  : 'contactController'
+			});
+	});
+
+	// create the controller and inject Angular's $scope
+	psrApp.controller('mainController', function($scope) {
+		// create a message to display in our view
+		$scope.message = 'Everyone come and see how good I look!';
+	});
+
+	psrApp.controller('aboutController', function($scope) {
+		$scope.message = 'Look! I am an about page.';
+	});
+
+	psrApp.controller('contactController', function($scope) {
+		$scope.message = 'Contact us! JK. This is just a demo.';
+	});
